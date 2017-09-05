893832b60b6f2c1c270ac5087002a4bdbab21922
Huy Tran <huytr402@student.liu.se>
2017-09-05 21:25:40 +0200

    rm dead code

--- utils/build.js
+++ utils/build.js
@@ -1,10 +1,6 @@
 const webpack = require("webpack");
 const config = require("../webpack.config");
 
-require("./prepare");
-
-delete config.chromeExtensionBoilerplate;
-
 webpack(
   config,
   (err) => { if (err) throw err; }
